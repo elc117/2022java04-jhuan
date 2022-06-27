8eb538c80c4316d982bdde121b1ba04ab2526635
Jhuan Assumpcao <jhuanassumpcao@gmail.com>
2022-06-27 16:30:44 -0300

    Create Eletronico.java

---
+++ Eletronico.java
@@ -0,0 +1,24 @@
+public class Eletronico {
+    private String nome;
+    private String hardware;
+    private int tamanho;
+    
+    //pega as caracteristicas dos eletronicos
+    public Eletronico(String nome,String hardware, int tamanho) {
+        this.nome = nome;
+        this.hardware = hardware;
+        this.tamanho = tamanho;
+    }
+    //pega hardware
+    public String getHardware() {
+        return hardware;
+    }
+    //pega o nome
+    public String getName() {
+        return nome;
+    }
+    //pega o tamanho
+    public int getTamanho() {
+        return tamanho;
+    }
+}
